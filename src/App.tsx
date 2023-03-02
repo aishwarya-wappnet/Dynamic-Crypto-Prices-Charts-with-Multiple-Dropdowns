48d833376352bdceaff527a4dc844653cd17684f
aishwarya <aishwarya.wappnet@gmail.com>
2023-03-02 19:23:42 +0530

    dynamic crypto prices chart with multiple dropdown

---
+++ src/App.tsx
@@ -0,0 +1,123 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import axios from 'axios';
+import CryptoSummary from './components/CryptoSummary';
+import { Crypto } from './Types';
+import { Line } from 'react-chartjs-2'; 
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
+import type { ChartData, ChartOptions } from 'chart.js';
+import moment from 'moment';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+// initial api requests with axios and typescript
+
+function App() {
+
+  const [cryptos, setCryptos] = useState<Crypto[] | null>();
+  const [selected, setSelected] = useState<Crypto | null>();
+  const [range, setRange] = useState<number>(30);
+  const [data, setData] = useState<ChartData<'line'>>();
+  const [options, setOptions] = useState<ChartOptions<'line'>>(
+    {
+      responsive: true,
+      plugins: {
+      legend: {
+        display: false
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Line Chart',
+      },
+    },
+    }
+  );
+
+
+  useEffect(() => {
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+  axios.get(url).then((response) => {
+    setCryptos(response.data);
+  });
+  }, [])
+
+  useEffect(() => {
+         // request
+        //  console.log(range)
+         axios.get(`https://api.coingecko.com/api/v3/coins/${selected?.id}/market_chart?vs_currency=usd&days=${range}&${range === 1 ? 'interval=hourly':'interval=daily'}`).then((response) => {
+          console.log(response.data);
+          // setdata
+          setData({
+            labels: response.data.prices.map((price: number[]) => {
+              return moment.unix(price[0]/1000).format(range === 1 ? 'HH:MM' : 'MM-DD');
+            }),
+            datasets: [
+              {
+                label: 'Dataset 1',
+                data: response.data.prices.map((price: number[]) => {
+                  return price[1];
+                }),
+                borderColor: 'rgb(255, 99, 132)',
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+              }
+            ]
+            })
+            setOptions({
+                responsive: true,
+                plugins: {
+                legend: {
+                  display: false
+                },
+                title: {
+                  display: true,
+                  text: `${selected?.name} Price Over Last ` + range + (range === 1 ? ` Day` : ` Days`)
+                },
+              },
+              }
+            )
+        })
+  }, [selected, range]);
+
+  return (
+    <>
+    <div className="App">
+
+      <select onChange={(e) => {
+        const c =  cryptos?.find((x) => x.id === e.target.value);
+        setSelected(c);
+        // update data state
+      }} defaultValue="default">
+        <option value='default'>Choose an option</option>
+      {cryptos ? cryptos.map((crypto) => {
+        // here, the key is actually for react, the value on the other hand is to decide which one was clicked. This allows us to get the target value inside of on on change
+        // Each <option> element should have a value attribute containing the data value to submit to the server when that option is selected. If no value attribute is included,
+        // the value defaults to the text contained inside the element.
+        return <option key={crypto.id} value={crypto.id}>{crypto.name}</option> 
+      }) : null }
+      </select>  
+      
+      <select onChange={(e) => {
+        setRange(parseInt(e.target.value))
+      }}>
+        <option value={30}>30 Days</option>
+        <option value={7}>7 Days</option>
+        <option value={1}>1 Day</option>
+      </select>
+
+    </div>
+    { selected ? <CryptoSummary crypto={selected} /> : null }
+    { data ? <div style={{ width: 600, margin: 'auto'}}><Line options={options} data={data} /></div> : null }
+    </>
+  )
+}
+
+export default App
---
+++ src/Types.tsx
@@ -0,0 +1,11 @@
+export type Crypto = {
+    ath: number;
+    atl: number;
+    current_price: number;
+    id: string
+    // image: "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"
+    name: string;
+    symbol: string;
+    high_24h: number;
+    low_24h: number;
+  }
